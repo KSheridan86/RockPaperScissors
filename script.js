64159aca15e074c55197d5488fd05c06c1e3ef91
Ken Sheridan <kensheridan86@gmail.com>
2023-03-19 11:39:27 +0000

    updated for week 9 labs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let winner = false;
 // Generate a random number and assign the computer's choice
 function computerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
@@ -12,31 +13,49 @@ function compareChoices(userChoice, computerChoice) {
         if (computerChoice === 'paper') {
             return "You lose! Paper covers rock.";
         } else {
+            winner = true;
             return "You win! Rock smashes scissors.";
         }
     } else if (userChoice === 'paper') {
         if (computerChoice === 'scissors') {
             return "You lose! Scissors cut paper.";
         } else {
+            winner = true;
             return "You win! Paper covers rock.";
         }
     } else if (userChoice === 'scissors') {
         if (computerChoice === 'rock') {
             return "You lose! Rock smashes scissors.";
         } else {
+            winner = true;
             return "You win! Scissors cut paper.";
         }
     }
 }
 
 // Grab the user's choice by the ID of the option they click and display the result
+let turns = 3;
+
 function handleInput() {
-    let userChoice = this.id;
-    let computer = computerChoice();
-    let result = compareChoices(userChoice, computer);
-    let playAgain = document.getElementById('play-again');
-    document.getElementById('result').innerHTML = result;
-    playAgain.style.display = 'block';
+    if (turns > 0) {
+        let userChoice = this.id;
+        let computer = computerChoice();
+        let result = compareChoices(userChoice, computer);
+        let playAgain = document.getElementById('play-again');
+        document.getElementById('result').innerHTML = result + "<br>" + `${turns - 1}` + "turns left";
+        playAgain.style.display = 'block';
+        if (turns === 1) {
+            playAgain.style.display = 'none';
+        }
+        turns--;
+        console.log(turns);
+        console.log(winner);
+        if (winner) {
+            turns = 0;
+            playAgain.style.display = 'none';
+            document.getElementById('result').innerHTML = result + "<br>" + "You win the game!";
+        }
+    }
 }
 
 // Attach click handlers to the game choices
